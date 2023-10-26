19b657c50566f585c82025a9b9d20270a8289254
박순국 <qkrtnsrnr@gmail.com>
2023-10-26 17:58:40 +0900

    레터박스 맍들어보고있는중

--- Assets/Scripts/WallManager.cs
+++ Assets/Scripts/WallManager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class WallManager : MonoBehaviour
 {
-    Collider2D[] walls; 
-    void Start()
+    public GameObject[] walls  ;
+    float wide;
+    void Awake()
     {
-        
+        //Left Wall
+        walls[0].gameObject.transform.SetPositionAndRotation(new Vector2(Camera.main.orthographicSize,Screen.width/2),new Quaternion(0,0,0,0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        wide = Screen.width / 2;
+        //Debug.Log(Screen.width / 2); //360
+
+        //Debug.Log(Camera.main.ScreenToWorldPoint());
     }
 }
